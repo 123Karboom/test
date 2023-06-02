ed9e6a3935f22b8520e2b446b554b2718ffdaf52
123Karboom <132123573+123Karboom@users.noreply.github.com>
2023-06-02 23:02:47 +0700

    push inxdex.cpp

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<setiosflags(ios::left)<<setw(20)<<"Nguyen Van A";
+    cout<<setiosflags(ios::left)<<setw(20)<<"Ha Noi";
+    cout<<setiosflags(ios::left)<<setw(35)<<"Sinh Vien";
+    cout<<endl;
+ 
+    cout<<setiosflags(ios::left)<<setw(20)<<"Nguyen Van Bom";
+    cout<<setiosflags(ios::left)<<setw(20)<<"Ho Chi Minh";
+    cout<<setiosflags(ios::left)<<setw(35)<<"Hoc Sinh";
+    cout<<endl;
+    return 0;
+} 
